d61a337924875d313020f7599f78551d0b1c3aac
Ujjwal <109898637+Ujjwal1178@users.noreply.github.com>
2023-02-04 17:17:24 +0530

    Add files via upload

---
+++ TreeSet_Collection_FWork.java
@@ -0,0 +1,34 @@
+
+import java.util.Set;
+import java.util.TreeSet ;
+
+public class TreeSet_Collection_FWork{
+    public static void main(String[] args) {
+        
+        Set<Integer> c = new TreeSet<>() ;
+        c.add(10) ;//add() function is used to insert the values in the set.
+        c.add(50) ;
+        c.add(100) ;
+        c.add(60) ;
+        System.out.println(c);// this will display the data in the form of sorted data .
+
+        //c.removeAll(c) ;// This will remove all the element from the set .
+        //System.out.println(c);
+
+        // remove() function is used to delete the element from the set.
+        c.remove(50) ;// this will remove 50 from the set .
+        System.out.println(c);
+
+
+        // contains() will return the boolean value, it returns true if value is present else false .
+        System.out.println( c.contains(60) );// returns true.
+
+        //size() function will returns the size of set.
+        System.out.println("Size is "+c.size());
+        
+        //clear() will clear our set.
+        c.clear();
+        System.out.println(c) ;
+
+    }
+}
